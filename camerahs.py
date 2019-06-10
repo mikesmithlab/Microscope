defa6cc1d7d04f2e8999e2b046d1f11ec7293931
ppzmis <mikesmithlab@gmail.com>
2019-06-10 15:27:29 +0100

    Initial Commit

--- camerahs.py
+++ camerahs.py
@@ -1,6 +1,18 @@
+from cam_settings import param_dict
+
+
 
 
 
 
 
 class CameraHS:
+
+    def __init__(self):
+
+    def _rewrite_mcf(self, param_dict):
+
+    def _write_value_camconfig(self, param_dict, value):
+
+    def _update_camera(self):
+
