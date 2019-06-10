636a468d6e0c0a74867ed6cc4191e22c0350ab9d
ppzmis <mikesmithlab@gmail.com>
2019-06-10 15:27:42 +0100

    Initial Commit

---
+++ cam_settings.py
@@ -0,0 +1,47 @@
+
+''''
+Reference for camera commmands can be found in cl600x2-SU-07-D_manual.pdf in microscope folder
+
+The dictionary values are (cam command, framegrabber command if appropriate)
+'''
+
+param_dict = {'serial':('#N',''),
+              'firmware':('#f',''),
+              'baudrate':('#B',''),
+              'gain':('#G',''),
+              'fpn':('#F',''),
+              'lut':('#L',''),
+              'updatelut':('#l',''),
+              'extsync':('#S',''),
+              'synchpol':('#P',''),
+              'synchmod':('#m',''),
+              'synchsrc':('#s',''),
+              'frameformat':('#R',''),
+              'framerate':('#r',''),
+              'maxframerate': ('#A',''),
+              'exptime': ('#e',''),
+              'maxexptime': ('#a',''),
+              'dualslope': ('#D',''),
+              'dualslopetime': ('#d',''),
+              'tripleslope': ('#T',''),
+              'tripleslopetime': ('#t',''),
+              'cameramode': ('#C',''),
+              'testimagemode': ('#I',''),
+              'currentmode': ('#v',''),
+              'startupmode': ('#V',''),
+              'recordonusermode': ('#u',''),
+              'resetcamera': ('#o',''),
+              'blacklevel': ('#z',''),
+              'camlinkdatafreq': ('#c', ''),
+              'numroi': ('#h',''),
+              'roistart': ('#H',''),
+              'nondestructivereadout': ('#k', ''),
+              'lasterror': ('#E',''),
+              'invert': ('#Q',''),
+              'calibration': ('#K',''),
+              'movingwindow': ('#M',''),
+              'camerastate': ('#X',''),
+              'framestampingmode': ('#Z','')
+
+
+}
