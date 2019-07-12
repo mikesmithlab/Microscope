70410fb420bf48fb6b218dcda2a43b93e846365c
ppzmis <mikesmithlab@gmail.com>
2019-07-12 15:47:17 +0100

    video editing gui nearly done but save button not working

--- camerahs.py
+++ camerahs.py
@@ -3,18 +3,18 @@ from shutil import copyfile
 import SiSoPyInterface as SISO
 import numpy as np
 import os
-from PyQt5.QtCore import Qt, pyqtSignal, QRectF, QT_VERSION_STR
+from PyQt5.QtCore import Qt, pyqtSlot
 from PyQt5.QtGui import QPixmap, QImage, QPainterPath, QCloseEvent
 from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QLabel, QApplication,
-                             QSlider, QHBoxLayout, QGraphicsView, QGraphicsScene,
-                             QFileDialog, QCheckBox)
+                             QSlider, QHBoxLayout, QPushButton)
+import qimage2ndarray as qim
 import sys
 
 from Generic.filedialogs import load_filename, save_filename, open_directory
 from Generic.file_handling import load_dict_from_file, save_dict_to_file
 from Generic.video import WriteVideo
 from Generic.images import display, write_img
-from Generic.images import QWidgetMod, QtImageViewer
+from Generic.images import QtImageViewer, hstack
 
 class Camera:
     def __init__(self, cam_config_dir='/opt/Microscope/ConfigFiles/'):
@@ -32,9 +32,10 @@ class Camera:
         SISO.SetBufferWidth(self.display, self.set.cam_dict['frameformat'][2][2],
                             self.set.cam_dict['frameformat'][2][3])
 
-    def grab(self, numpics=None):
+    def grab(self, numpics=20):
         if numpics is None:
             numpics = self.numPicsBuffer
+        self.numpics = numpics
 
         err = SISO.Fg_AcquireEx(self.fg, 0, numpics, SISO.ACQ_STANDARD, self.memHandle)
 
@@ -89,8 +90,9 @@ class Camera:
         self.stop()
 
     def stop(self):
-        self.max_img_num = 50#SISO.Fg_getLastPicNumberBlockingEx(self.fg, self.last_pic_nr, 0, 5, self.memHandle)
+        self.max_img_num = self.numpics#SISO.Fg_getLastPicNumberEx(self.fg, self.last_pic_nr, 0, 5, self.memHandle)
         SISO.Fg_stopAcquire(self.fg, 0)
+        self.edit()
 
     def edit(self):
         self.vid_edit = CameraVidEditGui(self)
@@ -129,17 +131,35 @@ class CameraControlGui:
         pass
 
 class CameraVidEditGui:
-    def __init__(self, cam):
+    def __init__(self, cam, num_imgs=2):
+        self.num_imgs = num_imgs
         self.cam=cam
-        self.first_frame = 0
+        self.first_frame = 1
         self.last_frame = self.cam.max_img_num
         self.edit_first_frame = self.first_frame
         self.edit_last_frame = self.last_frame
+        self.imgA = self.get_img(num=self.first_frame)
+        self.imgB = self.get_img(num=self.last_frame)
+        self._display_img(self.imgA, self.imgB)
+        self.init_ui()
+
+    def _display_img(self, *imgs):
+        self.img = hstack(*imgs)
+
+    def _update_img(self):
+        pixmap = QPixmap.fromImage(qim.array2qimage(self.img))
+        self.viewer.setImage(pixmap)
+
+    def get_img(self, num=1):
+        img_ptr = SISO.Fg_getImagePtrEx(self.cam.fg, num, 0, self.cam.memHandle)
+        nImg = SISO.getArrayFrom(img_ptr, self.cam.set.cam_dict['frameformat'][2][3],
+                                 self.cam.set.cam_dict['frameformat'][2][2])
+        return nImg
 
     def init_ui(self):
         # Create window and layout
         app = QApplication(sys.argv)
-        self.win = QWidgetMod(self.param_dict)
+        self.win = QWidget()
         self.vbox = QVBoxLayout(self.win)
 
         # Create Image viewer
@@ -149,30 +169,90 @@ class CameraVidEditGui:
         self.viewer.leftMouseButtonPressed.connect(self.get_coords)
         self.viewer.canZoom = True
         self.viewer.canPan = True
-        self._update_im()
+        self._update_img()
         self.vbox.addWidget(self.viewer)
 
-        # Create live update checkbox
-        cb = QCheckBox('Update')
-        cb.toggle()
-        cb.stateChanged.connect(self._update_cb)
-        self.live_update = True
-        self.vbox.addWidget(cb)
-
         # Add sliders
-        self.add_sliders()
+        self.add_sliderA(text='first')
+        self.add_sliderB(text='last', initial_val=self.last_frame)
+
+        self.add_button()
+
 
         # Finalise window
-        self.win.setWindowTitle('ParamGui')
+        self.win.setWindowTitle('VideoEditGui')
         self.win.setLayout(self.vbox)
         self.win.show()
         sys.exit(app.exec_())
 
-    def save(self):
-        pass
+    def add_sliderA(self, text='', initial_val=1):
+        widget = QWidget()
+        hbox = QHBoxLayout()
+        self.frame_lblA = QLabel()
+        self.frame_lblA.setText(text + ' frame: ' + str(initial_val))
 
-    def close(self):
-        pass
+        self.frame_sliderA = QSlider(Qt.Horizontal)
+        self.frame_sliderA.setRange(self.first_frame, self.last_frame)
+        self.frame_sliderA.setValue(initial_val)
+        self.frame_sliderA.valueChanged.connect(self._update_sliderA)
+        hbox.addWidget(self.frame_lblA)
+        hbox.addWidget(self.frame_sliderA)
+        widget.setLayout(hbox)
+        self.vbox.addWidget(widget)
+
+    def add_sliderB(self, text='', initial_val=1):
+        widget = QWidget()
+        hbox = QHBoxLayout()
+        self.frame_lblB = QLabel()
+        self.frame_lblB.setText(text + ' frame: ' + str(initial_val))
+        self.frame_sliderB = QSlider(Qt.Horizontal)
+        self.frame_sliderB.setRange(self.first_frame, self.last_frame)
+        self.frame_sliderB.setValue(initial_val)
+        self.frame_sliderB.valueChanged.connect(self._update_sliderB)
+        hbox.addWidget(self.frame_lblB)
+        hbox.addWidget(self.frame_sliderB)
+        widget.setLayout(hbox)
+        self.vbox.addWidget(widget)
+
+        #for key in sorted(self.param_dict.keys()):
+        #widget = QWidget()
+        #hbox = QHBoxLayout()
+
+    def add_button(self):
+        # Add Save Button
+        widget = QWidget()
+        hbox = QHBoxLayout()
+        self.saveButton = QPushButton("Save")
+        self.saveButton.clicked.connect(self.on_click)
+        hbox.addWidget(self.saveButton)
+        widget.setLayout(hbox)
+        self.vbox.addWidget(widget)
+
+
+    def _update_sliderA(self):
+        frameno = self.frame_sliderA.value()
+        self.frame_lblA.setText('frame: ' + str(frameno))
+        self.imgA = self.get_img(num=frameno)
+        self.update()
+
+    def _update_sliderB(self):
+        frameno = self.frame_sliderB.value()
+        self.frame_lblB.setText('frame: ' + str(frameno))
+        self.imgB = self.get_img(num=frameno)
+        self.update()
+
+    def update(self):
+        self._display_img(self.imgA, self.imgB)
+        self._update_img()
+
+    def get_coords(self, x, y):
+        print('cursor position (x, y) = ({}, {})'.format(int(x), int(y)))
+
+    @pyqtSlot()
+    def on_click(self):
+        print('test')
+        #self.cam.save(first=self.edit_first_frame, last=self.edit_last_frame)
+
 
 
 
@@ -301,7 +381,7 @@ if __name__ == '__main__':
 
     cam.initialise()
     cam.grab()
-    cam.save()
+    #cam.save()
     cam.release()
 
 
