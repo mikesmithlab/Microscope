30a884378186d016e2a0f5589b8076448d2f00d1
mike <mike.i.smith@nottingham.ac.uk>
2019-07-29 22:27:47 +0100

    video editing gui nearly done but save button not working

--- dmd2.py
+++ dmd2.py
@@ -18,11 +18,13 @@ class DMDGui:
         self.vbox = QVBoxLayout(self.win)
 
         led_chooser = LEDselector(self.win)
-        display_chooser = DisplaySelector(self.win, self.display_chooser_callback)
+        display_chooser = DisplaySelector(self.win)
+        rate_chooser = PatternRateSelector(self.win)
         file_chooser = FileSelector(self.win)
 
         self.vbox.addWidget(led_chooser)
         self.vbox.addWidget(display_chooser)
+        self.vbox.addWidget(rate_chooser)
         self.vbox.addWidget(file_chooser)
 
 
@@ -88,38 +90,35 @@ class LEDselector(QWidget):
 
     def red_led_val(self, redval):
         if self.red_led.check:
+            self.red_led.slider.setEnabled(True)
             self.redval = redval
         else:
-            self.redval = 0
-            self.red_led.slider.setSliderPosition(0)
+            self.red_led.slider.setEnabled(False)
 
     def green_led_val(self, greenval):
         if self.green_led.check:
+            self.green_led.slider.setEnabled(True)
             self.greenval = greenval
         else:
-            self.greenval = 0
-            self.green_led.slider.setSliderPosition(0)
+            self.green_led.slider.setEnabled(False)
 
     def blue_led_val(self, blueval):
         if self.blue_led.check:
+            self.blue_led.slider.setEnabled(True)
             self.blueval = blueval
         else:
-            self.blueval = 0
-            self.blue_led.slider.setSliderPosition(0)
+            self.blue_led.slider.setEnabled(False)
 
 
-    def change_led_settings(self, colour, value):
-        pass
 
 
 class DisplaySelector(QWidget):
 
-    def __init__(self, parent, function):
+    def __init__(self, parent):
         self.off = True
         self.on = False
         self.cycle = False
 
-        self.function = function
         QWidget.__init__(self, parent)
         self.setLayout(QHBoxLayout())
 
@@ -134,6 +133,8 @@ class DisplaySelector(QWidget):
         self.display_cycle = QPushButton("cycle")
         self.display_cycle.setCheckable(True)
         self.display_cycle.clicked[bool].connect(self.display_cycle_callback)
+
+        self.call_function()
 
         self.layout().addWidget(self.display_off)
         self.layout().addWidget(self.display_on)
@@ -161,22 +162,65 @@ class DisplaySelector(QWidget):
         self.display_off.setChecked(self.off)
         self.display_on.setChecked(self.on)
         self.display_cycle.setChecked(self.cycle)
-        self.function(self.off, self.on, self.cycle)
+        #self.function(self.off, self.on, self.cycle)
 
 class PatternRateSelector(QWidget):
-    def __init__(self, parent):
+    def __init__(self, parent, rate=100, exposure=10, triggered=False):
         QWidget.__init__(self, parent)
+        self.rate_val=str(rate)
+        self.exposure_val=str(exposure)
+        self.triggered_val=triggered
+
         self.directory = 'None Selected'
         self.parent = parent
-        self.setLayout(QHBoxLayout())
+        hbox = QHBoxLayout()
 
-        self.rate_edit = QLineEdit()
+        self.rate = QLineEdit()
+        self.rate.setText(self.rate_val)
+        self.rate.returnPressed.connect(self.rate_callback)
+        self.rate_lbl = QLabel('Rate (Hz)')
         self.exposure = QLineEdit()
+        self.exposure.setText(self.exposure_val)
+        self.exposure.returnPressed.connect(self.exposure_callback)
+        self.exposure_lbl = QLabel('Exposure (us)')
+        self.triggered = QCheckBox()
+        self.triggered.setChecked(self.triggered_val)
+        self.triggered.stateChanged.connect(self.triggered_callback)
+        self.triggered_lbl = QLabel('Triggered')
 
-        self.layout().addWidget(self.load_file_button)
-        self.layout().addWidget(self.load_file_label)
+        layout_rate = QVBoxLayout()
+        layout_exposure = QVBoxLayout()
+        layout_triggered = QVBoxLayout()
+        hbox.addLayout(layout_rate)
+        hbox.addLayout(layout_exposure)
+        hbox.addLayout(layout_triggered)
 
+        layout_rate.addWidget(self.rate_lbl)
+        layout_rate.addWidget(self.rate)
+        layout_exposure.addWidget(self.exposure_lbl)
+        layout_exposure.addWidget(self.exposure)
+        layout_triggered.addWidget(self.triggered_lbl)
+        layout_triggered.addWidget(self.triggered)
 
+        self.setLayout(hbox)
+
+    def rate_callback(self):
+        value = self.rate.text()
+        print(value)
+
+    def exposure_callback(self):
+        value = self.exposure.text()
+        print(value)
+
+    def triggered_callback(self, state):
+        self.triggered_val = bool(self.triggered.checkState())
+        print(self.triggered_val)
+        if self.triggered_val:
+            self.rate.setEnabled(False)
+        else:
+            self.rate.setEnabled(True)
+
+        
 
 class FileSelector(QWidget):
     def __init__(self, parent):
